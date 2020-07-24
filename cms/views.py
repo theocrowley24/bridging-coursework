8f6aa715c889748985aee517fd95ad5a1324274b
Theo <theocrowley24@gmail.com>
2020-07-24 13:11:49 +0100

    Added view for post detail

--- cms/views.py
+++ cms/views.py
@@ -7,3 +7,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'cms/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'cms/post_detail.html', {'post': post})
